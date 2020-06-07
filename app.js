bae0509802a1e9cf4564dc58329d8f6995a25e4b
tutu-tonton <tngt33844507@gmail.com>
2020-06-07 13:16:27 +0900

    lesson3-firestore内のドキュメントデータを取得する方法

---
+++ app.js
@@ -0,0 +1,40 @@
+const cafeList = document.querySelector('#cafe-list');
+
+// create element and render cafe
+const renderCafe = (doc) => {
+	let li = document.createElement('li');
+	let name = document.createElement('span');
+	let city = document.createElement('span');
+
+	li.setAttribute('data-id', doc.id);
+	name.textContent = doc.data().name;
+	city.textContent = doc.data().city;
+
+	li.appendChild(name);
+	li.appendChild(city);
+
+	cafeList.appendChild(li);
+};
+
+// dbのドキュメントデータを取得
+db.collection('cafes')
+	.get()
+	.then((snapshot) => {
+		snapshot.docs.forEach((doc) => {
+			renderCafe(doc);
+		});
+	});
+
+//========================================
+//  dbのドキュメントデータを取得
+//
+//
+//
+//========================================
+// db.collection('cafes')
+// 	.get()
+// 	.then((snapshot) => {
+// 		snapshot.docs.forEach((doc) => {
+// 			console.log(doc.data());
+// 		});
+// 	});
