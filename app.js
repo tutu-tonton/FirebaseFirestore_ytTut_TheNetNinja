6ff021292ebee73b7d1f05b2df2796a92e3f4a72
tutu-tonton <tngt33844507@gmail.com>
2020-06-07 17:19:24 +0900

    lesson4 saving data

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const cafeList = document.querySelector('#cafe-list');
+const form = document.querySelector('#add-cafe-form');
 
 // create element and render cafe
 const renderCafe = (doc) => {
@@ -24,6 +25,17 @@ db.collection('cafes')
 			renderCafe(doc);
 		});
 	});
+
+// saving data
+form.addEventListener('submit', (e) => {
+	e.preventDefault();
+	db.collection('cafes').add({
+		name: form.name.value,
+		city: form.city.value,
+	});
+	form.name.value = '';
+	form.city.value = '';
+});
 
 //========================================
 //  dbのドキュメントデータを取得
